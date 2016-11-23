818955f79db1ed7dad8e270f3ed3cfa8a75d802f
Dan Riding <doonyby@gmail.com>
2016-11-23 14:48:48 -0700

    added animation

---
+++ app/owm-app.js
@@ -0,0 +1,45 @@
+angular.module('OWMApp', ['ngRoute', 'ngAnimate'])
+	.value('owmCities', ['New York', 'Dallas', 'Chicago'])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl : 'home.html',
+            controller : 'HomeCtrl',
+            controllerAs: 'vm'
+        }).when('/cities/:city', {
+		    templateUrl : 'city.html',
+		    controller : 'CityCtrl',
+		    controllerAs: 'vm',
+		    resolve : {
+		        city: function(owmCities, $route, $location) {
+		            var city = $route.current.params.city;
+		            if(owmCities.indexOf(city) === -1 ) {
+		                $location.path('/error');
+		                return;
+		            }
+		            return city;
+		        }
+		    }
+		}).when('/error', {
+		    template : '<p>Error - Page Not Found</p>'
+		}).otherwise('/error');
+    }])
+	.run(function($rootScope, $location, $timeout) {
+	    $rootScope.$on('$routeChangeError', function() {
+	        $location.path("/error");
+	    });
+	    $rootScope.$on('$routeChangeStart', function() {
+	        $rootScope.isLoading = true;
+	    });
+	    $rootScope.$on('$routeChangeSuccess', function() {
+	      $timeout(function() {
+	        $rootScope.isLoading = false;
+	      }, 1000);
+	    });
+	})
+    .controller('HomeCtrl', [function() {
+        //empty for now
+    }])
+	.controller('CityCtrl', ['city', function(city) {
+	    var vm = this;
+	    vm.city = city;
+	}]);
